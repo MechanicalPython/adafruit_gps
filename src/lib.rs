f7b2d6d62980283595c6041c4eb51bd474cb0214
Matt Barson <matt.barson@gmail.com>
2020-03-25 15:08:59 +0000

    Test serial port.

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,34 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
 extern crate serial;
 
-use std::{env, io, time};
+use std::env;
+use std::io;
+use std::time::Duration;
+
+use std::io::prelude::*;
 use serial::prelude::*;
 
 
 
 fn read_serial_port() {
     let port_name = "/dev/serial0";
-    let port = serial::open(port_name);
-    serial::
-    port.read()
+    let mut port = serial::open(port_name).unwrap();
+    interact(&mut port).unwrap();
+
+
 }
 
 fn interact<T: SerialPort>(port: &mut T) -> io::Result<()> {
-
-    settings.set_baud_rate(serial::Baud9600);
-    settings.set_char_size(serial::Bits8);
-    settings.set_parity(serial::ParityNone);
-    settings.set_stop_bits(serial::Stop1);
-    settings.set_flow_control(serial::FlowNone);
+    (port.reconfigure(&|settings| {
+        (settings.set_baud_rate(serial::Baud9600));
+        settings.set_char_size(serial::Bits8);
+        settings.set_parity(serial::ParityNone);
+        settings.set_stop_bits(serial::Stop1);
+        settings.set_flow_control(serial::FlowNone);
+        Ok(())
+    }));
 
     port.set_timeout(Duration::from_millis(1000));
 
@@ -26,5 +36,7 @@ fn interact<T: SerialPort>(port: &mut T) -> io::Result<()> {
 
     port.write(&buf[..]);
     port.read(&mut buf[..]);
+    println!("{:?}", buf);
+    Ok(())
 
 }

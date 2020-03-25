00b364b3f0c2681cdca790ca0c076624ae57deba
Matt Barson <matt.barson@gmail.com>
2020-03-25 16:52:38 +0000

    test

--- src/lib.rs
+++ src/lib.rs
@@ -11,34 +11,15 @@ use serialport::prelude::*;
 
 pub fn read_serial_port() {
     let port_name = "/dev/serial0";
-    let baud_rate = "9600";
+    let baud_rate:u32 = 9600;
 
     let mut settings: SerialPortSettings = Default::default();
     settings.timeout = Duration::from_millis(1000);
+    settings.baud_rate = baud_rate;
 
-    if let Ok(rate) = baud_rate.parse::<u32>() {
-        settings.baud_rate = rate.into();
-    } else {
-        eprintln!("Error: Invalid baud rate '{}' specified", baud_rate);
-        ::std::process::exit(1);
-    }
-
-    match serialport::open_with_settings(&port_name, &settings) {
-        Ok(mut port) => {
-            let mut serial_buf: Vec<u8> = vec![0; 1000];
-            loop {
-                match port.read(serial_buf.as_mut_slice()) {
-                    Ok(t) => io::stdout().write_all(&serial_buf[..t]).unwrap(),
-                    Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
-                    Err(e) => eprintln!("{:?}", e),
-                }
-            }
-        }
-
-        Err(e) => {
-            eprintln!("Failed to open \"{}\". Error: {}", port_name, e);
-            ::std::process::exit(1);
-        }
-    }
+    let port = serialport::open_with_settings(&port_name, &settings);
+    let mut serial_buf: Vec<u8> = vec![0; 1000];
+    port.read(serial_buf.as_mut_slice());
+    println!("{}", port);
 
 }

055a9154333c0d14a8966df2f8c3972c888a42cc
Matt Barson <matt.barson@gmail.com>
2020-03-27 21:35:52 +0000

    Roll back to funtion only.

--- src/bin.rs
+++ src/bin.rs
@@ -1,7 +1,18 @@
+// use std::fs::File;
+// use std::io::Read;
 
 use mylib;
+// use std::str::from_utf8;
+// use std::i64;
 
-pub fn main() {
-    mylib::read_serial_port();
+#[allow(unused_imports)]
+#[allow(unused_variables)]
+
+fn main() {
+    let gps = mylib::read_serial_port("/dev/serial0");
+    println!("{:?}", gps);
+
+
+
 
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,23 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
-
+/// General structure
+/// GPS enum has all the items that are needed.
+///
+///
+///
+///
 extern crate serialport;
 
+use std::i64;
+use std::io::{self, Read, Write};
 use std::str;
-use std::io::{self, Write, Read};
-use std::time::{SystemTime, Duration};
+use std::thread::sleep;
+use std::time::{Duration, SystemTime};
 
 use serialport::prelude::*;
-use std::thread::sleep;
 
-fn vec_to_str(v:Vec<u8>) {
-    let s = str::from_utf8(&v).unwrap();
-    println!("{}", s);
-}
-
-pub fn read_serial_port() {
-    let port_name = "/dev/serial0";
+pub fn read_serial_port(port_name: &str) -> Vec<u8> {
+    // let port_name = "/dev/serial0";
 
     let settings = SerialPortSettings {
         baud_rate: 9600,
@@ -26,43 +27,19 @@ pub fn read_serial_port() {
         stop_bits: StopBits::One,
         timeout: Duration::from_millis(1000),
     };
+
     let mut port = serialport::open_with_settings(&port_name, &settings).unwrap();
-    let mut buffer:Vec<u8> = vec![0;1000];
-    let mut sentence:Vec<u8> = Vec::new();
+    let mut buffer: Vec<u8> = vec![0; 1000];
+    let mut output: Vec<u8> = Vec::new();
 
     let s = SystemTime::now();
     while s.elapsed().unwrap() < Duration::from_secs(1) {
         match port.read(buffer.as_mut_slice()) {
             Ok(_t) => {
-                sentence.extend_from_slice(&buffer[.._t]);
-                // println!("{:?}", buffer)
-            },
-            Err(e) => (eprint!("{:?}\n", e)),
+                output.extend_from_slice(&buffer[.._t]);
+            }
+            Err(_e) => (),
         }
     }
-    println!("{:?}", vec_to_str(sentence));
-
-    // match serialport::open_with_settings(&port_name, &settings) {
-    //     Ok(mut port) => { // Port is now open.
-    //         let mut buffer: Vec<u8> = vec![0;1000];
-    //         loop {
-    //             // So create a massive vector. The port.read() method then changes the values
-    //             // in that vector and returns t, the number of valid bytes (non 0).
-    //             match port.read(buffer.as_mut_slice()) {
-    //                 Ok(_t) => {
-    //                     println!("{:?} -- {:?}\n", &buffer[..t], _t);
-    //
-    //                 }
-    //
-    //                 ,
-    //
-    //                 Err(e) => (eprint!("{:?}\n", e)),
-    //             }
-    //         }
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Failed to open \"{}\". Error: {}", port_name, e);
-    //         ::std::process::exit(1);
-    //     }
-    // }
+    return output;
 }

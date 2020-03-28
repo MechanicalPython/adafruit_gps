f7bff9d7e6c4b9c60172ca71fd3834a7f8347561
Matt Barson <matt.barson@gmail.com>
2020-03-28 15:36:02 +0000

    read full lines from buffer.

--- src/bin.rs
+++ src/bin.rs
@@ -1,18 +1,22 @@
-// use std::fs::File;
-// use std::io::Read;
-use std::thread::sleep;
-use std::time::{Duration};
-
-use mylib::Gps;
 
 #[allow(unused_imports)]
 #[allow(unused_variables)]
+
+// use std::fs::File;
+// use std::io::Read;
+// use std::thread::sleep;
+// use std::time::{Duration};
+// use std::str;
+use mylib::Gps;
+
+
 
 fn main() {
     let mut gps = Gps{
         port: mylib::open_port("/dev/serial0"),
     };
-    sleep(Duration::from_secs(60));
+
     &gps.parse_sentence();
+
 
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,9 @@
 #![allow(unused_imports)]
 /// General structure
 /// GPS enum has all the items that are needed.
-///
+/// The way it works. Constantly call gps.update(). This will update the variables with the
+/// most up to date items (each type of prefix indicates a different level of importance)
+/// And then every second print the most up to date info.
 
 extern crate serialport;
 
@@ -15,44 +17,59 @@ use std::time::{Duration, SystemTime};
 use serialport::prelude::*;
 
 pub fn open_port(port_name: &str) -> Box<dyn SerialPort> {
-        let settings = SerialPortSettings {
-            baud_rate: 9600,
-            data_bits: DataBits::Eight,
-            flow_control: FlowControl::None,
-            parity: Parity::None,
-            stop_bits: StopBits::One,
-            timeout: Duration::from_millis(1000),
-        };
-        match serialport::open_with_settings(&port_name, &settings) {
-            Ok(port) => return port,
-            Err(_e) => panic!("Port not found."),
-        }
+    let settings = SerialPortSettings {
+        baud_rate: 9600,
+        data_bits: DataBits::Eight,
+        flow_control: FlowControl::None,
+        parity: Parity::None,
+        stop_bits: StopBits::One,
+        timeout: Duration::from_millis(1000),
+    };
+    match serialport::open_with_settings(&port_name, &settings) {
+        Ok(port) => return port,
+        Err(_e) => panic!("Port not found."),
     }
+}
 
 pub struct Gps {
     pub port: Box<dyn SerialPort>,
 }
 
 impl Gps {
-    pub fn read_port(&mut self) -> Vec<u8> {
+    fn read_line(&mut self) -> Vec<u8> {
         // Maximum port buffer size is 4095.
         // Returns whatever is in the port.
-        // Start of a line is $ and end is \n. So
-        let mut buffer: Vec<u8> = vec![0; 4095+100];  // Reads what is in the buffer.
+        // Start of a line is $ (36) and end is \n (10). So if
+        // The correct line length is 70 (probably).
+        let mut buffer: Vec<u8> = vec![0; 4095];  // Reads what is in the buffer, be it nothing or max.
         let mut output: Vec<u8> = Vec::new();
         let p = &mut self.port;
-
-        match p.read(buffer.as_mut_slice()) {
-            Ok(_t) => {
-                println!("{:?}", buffer);
-                output.extend_from_slice(&buffer[.._t]);
+        let mut cont = true;
+        while cont {
+            match p.read(buffer.as_mut_slice()) {
+                Ok(buffer_size) => {
+                    output.extend_from_slice(&buffer[..buffer_size]);
+                    while output.get(0).unwrap() != &36u8 {  // Remove all characters before $
+                        output.remove(0);
+                    }
+                    if buffer[..buffer_size].contains(&10u8) {
+                        cont = false;
+                        while output.get(output.len() - 1).unwrap() != &10u8 {
+                            output.remove(output.len() - 1);
+                        }
+                    }
+                }
+                Err(_e) => (),
             }
-            Err(_e) => (),
         }
         return output;
     }
-    pub fn parse_sentence(&mut self) -> (){
-        let port_reading = self.read_port();
+
+    pub fn update(self) {}
+
+    pub fn parse_sentence(&mut self) -> () {
+        // Return only the last valid line.
+        let port_reading = self.read_line();
         let string = str::from_utf8(&port_reading).unwrap();
         println!("{:?}", string);
     }

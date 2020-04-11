4cb2ff27e0d7ecf710e166c8b0561cfdda43ece4
Matt Barson <matt.barson@gmail.com>
2020-04-11 13:34:52 +0100

    testing.

--- src/bin.rs
+++ src/bin.rs
@@ -1,5 +1,7 @@
 use std::str;
 use std::env;
+use std::thread;
+use std::time::Duration;
 
 use adafruit_gps::{Gps, open_port};
 use std::io::Read;
@@ -8,12 +10,17 @@ fn main() {
     let args: Vec <String> = env::args().collect();
     let cmd = &args[1];
 
-    let mut serial_buf= String::new();
+    let mut serial_buf: Vec<u8> = vec![0;1000];
     let mut gps = Gps { port: open_port("/dev/serial0") };
+    thread::sleep(Duration::from_secs(1));
     let bytes_to_read = gps.port.bytes_to_read();
     println!("{:?}", bytes_to_read);
-    let r = gps.port.read_to_string(&mut serial_buf).unwrap();
+
+    let r = gps.port.read(&mut serial_buf).unwrap();
     println!("{:?}", r);
+
+    let bytes_to_read = gps.port.bytes_to_read();
+    println!("{:?}", bytes_to_read);
 
 
 

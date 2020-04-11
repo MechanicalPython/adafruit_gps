357ba2748613f169e8bfb07cbce036bfb0c512e9
Matt Barson <matt.barson@gmail.com>
2020-04-11 16:13:16 +0100

    when does cmd send reply go in buffer.

--- src/bin.rs
+++ src/bin.rs
@@ -1,15 +1,10 @@
-use std::env;
-use std::io::Read;
 use std::str;
-// use std::env;
 use std::thread;
 use std::time::Duration;
 
 use adafruit_gps::{Gps, open_port};
 
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    // let cmd: &usize = &args[1].parse::<usize>().unwrap();
 
     // What cannot fit into the buffer is not read. Reads from the top down. Least recent to most recent.
     // Always read from the top down
@@ -17,9 +12,9 @@ fn main() {
     let mut gps = Gps { port: open_port("/dev/serial0") };
 
 
-    thread::sleep(Duration::from_secs(5));
+    thread::sleep(Duration::from_secs(2));
     gps.send_command("PMTK010,001");
-    for _i in 0..50 {
+    for _i in 0..5 {
 
         let lines = gps.read_line();
         let string: Vec<&str> = str::from_utf8(&lines).unwrap().split("\n").collect();

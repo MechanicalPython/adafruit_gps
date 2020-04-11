29c64e32a3df0a4b20ecd99405f6f66b244694a5
Matt Barson <matt.barson@gmail.com>
2020-04-11 13:39:20 +0100

    testing.

--- src/bin.rs
+++ src/bin.rs
@@ -1,27 +1,35 @@
+use std::io::Read;
 use std::str;
-use std::env;
+// use std::env;
 use std::thread;
 use std::time::Duration;
 
 use adafruit_gps::{Gps, open_port};
-use std::io::Read;
 
 fn main() {
-    let args: Vec <String> = env::args().collect();
-    let cmd = &args[1];
+    // let args: Vec <String> = env::args().collect();
+    // let cmd = &args[1];
 
-    let mut serial_buf: Vec<u8> = vec![0;1000];
+    let mut buffer: Vec<u8> = vec![0; 4095];  // Reads what is in the buffer, be it nothing or max.
+    let mut output: Vec<u8> = Vec::new();
+
     let mut gps = Gps { port: open_port("/dev/serial0") };
     thread::sleep(Duration::from_secs(1));
-    let bytes_to_read = gps.port.bytes_to_read();
-    println!("{:?}", bytes_to_read);
-
-    let r = gps.port.read(&mut serial_buf).unwrap();
-    println!("{:?}", r);
 
     let bytes_to_read = gps.port.bytes_to_read();
     println!("{:?}", bytes_to_read);
 
+    match gps.port.read(buffer.as_mut_slice()) {
+        Ok(buffer_size) => {
+            output.extend_from_slice(&buffer[..buffer_size]);
+        }
+        Err(_e) => (),
+    }
+    let string:Vec<&str> = str::from_utf8(&output).unwrap().split("\n").collect();
+    println!("{:?}", string);
+
+    let bytes_to_read = gps.port.bytes_to_read();
+    println!("{:?}", bytes_to_read);
 
 
     // // Turn on the basic GGA and RMC info (what you typically want)
@@ -29,5 +37,4 @@ fn main() {
     // let line:Vec <u8> = gps.read_line();
     // let line: Vec <&str> = str::from_utf8(&line).unwrap().split("\n").collect();
     // println!("{:?}", line);
-
 }

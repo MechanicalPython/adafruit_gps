68045e71cfd7986782301b4826f9a7885ca390a5
Matt Barson <matt.barson@gmail.com>
2020-04-12 18:03:13 +0100

    testing

--- src/bin.rs
+++ src/bin.rs
@@ -3,14 +3,21 @@ use std::str;
 use std::thread;
 use std::time::Duration;
 
-use adafruit_gps::{Gps, GpsArgValues, open_port, SendPmtk};
+use adafruit_gps::{Gps, GpsArgValues, open_port, SendPmtk, GetData};
 
 fn main() {
     let args:Vec<String> = env::args().collect();
-    let cmd:&String = args.get(1).expect("No command given.");
     let mut port = open_port("/dev/serial0");
     let mut gps = Gps{port, gps_type: "MT3339" };
-    thread::sleep(Duration::from_secs(1));
-    gps.send_command(format!("{}", cmd).as_str(), true);
+
+    if args.len() == 2 {
+        let cmd:&String = args.get(1).expect("No command given.");
+        gps.send_command(format!("{}", cmd).as_str(), true);
+    } else {
+        let l = gps.read_line();
+        println!("{}", l)
+    }
+
+
 
 }

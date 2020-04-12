714124b624d9beeee5ead2e215b1f8c63bba091d
Matt Barson <matt.barson@gmail.com>
2020-04-12 12:22:22 +0100

    change cmd.

--- src/bin.rs
+++ src/bin.rs
@@ -9,7 +9,7 @@ fn main() {
     let mut port = open_port("/dev/serial0");
     let mut gps = Gps{port, gps_type: "MT3339" };
     thread::sleep(Duration::from_secs(1));
-    gps.send_command("$PMTK220,1000", true);
+    gps.send_command("$PMTK010,1", true);
 
 
 }

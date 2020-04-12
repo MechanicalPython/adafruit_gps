0756a950008a106bab7c20d2fa090c329d33de16
Matt Barson <matt.barson@gmail.com>
2020-04-12 16:35:42 +0100

    debug

--- src/bin.rs
+++ src/bin.rs
@@ -9,6 +9,7 @@ use adafruit_gps::{Gps, GpsArgValues, open_port, SendPmtk};
 
 fn main() {
     let args:Vec<String> = env::args().collect();
+    dbg!(&args);
     let cmd:&String = args.get(1).expect("No command given.");
     let mut port = open_port("/dev/serial0");
     let mut gps = Gps{port, gps_type: "MT3339" };

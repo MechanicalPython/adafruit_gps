464e36c90c6543bafad242aa3599e97e2d2817a1
Matt Barson <matt.barson@gmail.com>
2020-04-12 12:18:16 +0100

    sleep

--- src/bin.rs
+++ src/bin.rs
@@ -8,6 +8,7 @@ use adafruit_gps::{Gps, GpsArgValues, open_port, SendPmtk};
 fn main() {
     let mut port = open_port("/dev/serial0");
     let mut gps = Gps{port, gps_type: "MT3339" };
+    thread::sleep(Duration::from_secs(1));
     gps.send_command("$PMTK220,1000", true);
 
 

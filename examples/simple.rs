b8835d6bf77eee1f5e00e714c59c6b374168d76b
Matt Barson <matt.barson@gmail.com>
2020-04-19 13:39:02 +0100

    Pretty print.

--- examples/simple.rs
+++ examples/simple.rs
@@ -15,21 +15,21 @@ fn main() {
     loop {
         let values = gps.update();
         let pretty_print = format!("\
-        utc: {}\
-        lat:  {:?}\
-        long: {:?}\
-        alt:  {:?}\
-        course true:{:?}\
-        course mag: {:?}\
-        knots: {:?}\
-        kph:   {:?}\
-        geo: {:?}\
-        age: {:?}\
-        sats: {:?}\
-        hdop: {:?}\
-        vdop: {:?}\
-        pdop: {:?}\
-        satellites: {:?}\
+        utc: {}\n
+        lat:  {:?}\n
+        long: {:?}\n
+        alt:  {:?}\n
+        course true:{:?}\n
+        course mag: {:?}\n
+        knots: {:?}\n
+        kph:   {:?}\n
+        geo: {:?}\n
+        age: {:?}\n
+        sats: {:?}\n
+        hdop: {:?}\n
+        vdop: {:?}\n
+        pdop: {:?}\n
+        satellites: {:?}\n\n
         ", values.utc, values.latitude, values.longitude, values.altitude, values.true_course,
         values.mag_course, values.speed_knots, values.speed_kph, values.geoidal_spe, values.age_diff_corr,
         values.sats_used, values.hdop, values.vdop, values.pdop, values.satellites);

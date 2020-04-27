ce2a67e8b7e8f1c89aa9a69454ca7e31d6863d57
Matt Barson <matt.barson@gmail.com>
2020-04-27 19:36:29 +0100

    debug

--- examples/debug.rs
+++ examples/debug.rs
@@ -15,7 +15,7 @@ fn main() {
     gps.pmtk_314_api_set_nmea_output(0, 0, 1, 1, 1, 1, 1);
     // Recommended gps update rate 1000miliseconds, or 1Hz.
     let r = gps.pmtk_605_q_release();
-    gps.pmtk_220_set_nmea_updaterate("500");
+    gps.pmtk_220_set_nmea_updaterate("100");
     dbg!(r);
 
 

7baa474bbb8d0726613dcd47add1a96c8d703bab
Matt Barson <matt.barson@gmail.com>
2020-04-29 13:43:23 +0100

    Warn advanced.rs panic.

--- examples/advanced.rs
+++ examples/advanced.rs
@@ -9,8 +9,6 @@ fn main() {
     let mut gps = Gps {port};
 
     gps.pmtk_314_api_set_nmea_output(0,0,0,0,0,1,1);
-    // Recommended gps update rate 1000miliseconds, or 1Hz.
-    gps.pmtk_220_set_nmea_updaterate("500");
 
     // Here you can read your own line and parse it how you like.
     // Note, that once a line is read it is gone. If you don't parse it and then drop the variable
@@ -21,7 +19,9 @@ fn main() {
         // Convert the String to a Vec<&str>
         let line = nmea::nmea::parse_sentence(line.as_str()).unwrap();
         // Parse the Vec<&str> to parse_gsa and return the GsaData struct.
+
         let _gsa = nmea::gsa::parse_gsa(line);
+        // Will panic as the line could be a PMTK reply.
 
     }
 }

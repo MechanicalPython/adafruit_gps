2a34472a92f55827c60c4f2e63f54eb203c8eb26
Matt Barson <matt.barson@gmail.com>
2020-04-30 16:58:32 +0100

    debugging

--- examples/test_update_rate.rs
+++ examples/test_update_rate.rs
@@ -4,6 +4,9 @@ use std::env;
 
 pub use adafruit_gps::gps::{GetGpsData, Gps, open_port};
 use adafruit_gps::PMTK::send_pmtk::SendPmtk;
+use std::io::Write;
+use std::time::Duration;
+use std::thread;
 
 // For use in testing your gps modules update rate. type the update rate in miliseconds in the cmd line.
 
@@ -11,6 +14,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     // args: hz, baudrate for port, baudrate for gps.
     let baud_rate = args.get(2).unwrap();
+    {
+        let port = open_port("/dev/serial0", 9600);
+        let mut gps = Gps { port , satellite_data: true, naviagtion_data: true };
+        gps.pmtk_251_set_nmea_baudrate(baud_rate);
+        thread::sleep(Duration::from_secs(1))
+    }
     let port = open_port("/dev/serial0", baud_rate.parse::<u32>().unwrap());
     println!("{:?}", port.baud_rate());
     let mut gps = Gps { port , satellite_data: true, naviagtion_data: true };

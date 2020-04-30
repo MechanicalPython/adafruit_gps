cdedf90d58fd38013112be4c6968873e49624810
Matt Barson <matt.barson@gmail.com>
2020-04-30 14:22:51 +0100

    test update rate example now accepts baudrate option.

--- examples/test_update_rate.rs
+++ examples/test_update_rate.rs
@@ -10,7 +10,9 @@ use adafruit_gps::PMTK::send_pmtk::SendPmtk;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let port = open_port("/dev/serial0");
+    let baud_rate = args.get(2).unwrap();
+    let baud_rate: u32 = baud_rate.parse().unwrap();
+    let port = open_port("/dev/serial0", baud_rate);
     let mut gps = Gps { port };
 
     let update_r = gps.pmtk_220_set_nmea_updaterate(&args[1]);
@@ -18,9 +20,6 @@ fn main() {
 
     for _ in 0..10 {
         let values = gps.update();
-        // let _pretty_print = format!("utc:{},lat:{:?},long:{:?}, alt:{:?}, course true:{:?}, course mag:{:?}, knots:{:?}, kph:{:?}, geo:{:?}, age:{:?}, sats:{:?}, hdop:{:?}, vdop:{:?}, pdop:{:?}, satellites:{:?}\n", values.utc, values.latitude, values.longitude, values.altitude, values.true_course,
-        // values.mag_course, values.speed_knots, values.speed_kph, values.geoidal_spe, values.age_diff_corr,
-        // values.sats_used, values.hdop, values.vdop, values.pdop, values.satellites);
         println!("{}", values.utc);
     }
 }

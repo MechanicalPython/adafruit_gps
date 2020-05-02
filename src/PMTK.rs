175057f6287e89c01cea44c853d58af965cbf95c
Matt Barson <matt.barson@gmail.com>
2020-05-02 13:44:56 +0100

    print

--- src/PMTK.rs
+++ src/PMTK.rs
@@ -183,7 +183,7 @@ pub mod send_pmtk {
                     Err(_e) => (),
                 }
             }
-            println!("{:?}", output);
+            println!("{:?}", port.baud_rate());
             let string: String = str::from_utf8(&output).unwrap_or("Invalid bytes given").to_string();
             println!("{}", string);
             if string != "Invalid bytes given".to_string() {

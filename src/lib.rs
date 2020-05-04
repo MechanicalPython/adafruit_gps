b448768ecf8671e81f9bd499758ad57f39e1be58
Matt Barson <matt.barson@gmail.com>
2020-05-04 19:40:38 +0100

    debug

--- src/lib.rs
+++ src/lib.rs
@@ -173,6 +173,7 @@ pub mod gps {
             while cont {
                 // If there is no connection, this match statement is looped over with a 1 second time out
                 // as given by the port open.
+                println!("here");
                 if start.duration_since(start).unwrap() > Duration::from_secs(1) {
                     println!("No connection from read line");
                     return PortConnection::NoConnection;
